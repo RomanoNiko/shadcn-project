70939e8dcd3b7332a890a16c589cb0836889b361
main <222011356@stis.ac.id>
2025-02-18 13:32:28 +0700

    tes

--- pages/documentation/getting-started/introduction.vue
+++ pages/documentation/getting-started/introduction.vue
@@ -18,5 +18,23 @@ definePageMeta({
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur
         </p>
+        <h2 class="scroll-m-20 text-2xl font-bold tracking-tight pb-3">Tes1</h2>
+        <p class="text-lg text-muted-foreground">
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur
+        </p>
+        <h2>Tes2</h2>
+        <p>
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur
+        </p>
     </div>
 </template>
