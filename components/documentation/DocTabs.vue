3461f7e7ae9561be90aff99cc3e2b3379940f385
main <222011356@stis.ac.id>
2025-02-20 11:16:36 +0700

    adding component

---
+++ components/documentation/DocTabs.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+</script>
+
+<template>
+    <Tabs default-value="preview" class="w-full">
+        <TabsList class="grid w-full grid-cols-2">
+            <TabsTrigger value="preview"> Preview </TabsTrigger>
+            <TabsTrigger value="code"> Code </TabsTrigger>
+        </TabsList>
+        <TabsContent value="preview">
+            <slot name="preview"></slot>
+        </TabsContent>
+        <TabsContent value="code">
+            <slot name="code"></slot>
+        </TabsContent>
+    </Tabs>
+</template>
---
+++ components/documentation/PreviewCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Card, CardContent } from "@/components/ui/card";
+</script>
+
+<template>
+    <Card>
+        <CardContent
+            class="flex justify-center items-center p-4 border rounded bg-gray-100 min-h-[200px]"
+        >
+            <slot></slot>
+        </CardContent>
+    </Card>
+</template>
